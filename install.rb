abce9aabfea1236e5c2ee9f2175052ef5facd92b
Keith Thibodeaux <kthibodeaux@osx2012021501.peachtree.local>
2014-06-03 11:00:40 -0400

    Initial commit

---
+++ install.rb
@@ -0,0 +1,16 @@
+files = [
+  '.gitconfig',
+  '.gitmessage',
+  '.rspec'
+]
+
+files.each do |file|
+  target_file = File.expand_path("~/#{ file }")
+  source_file = File.expand_path("~/.dotfiles/#{ file }")
+
+  puts "skipping #{ target_file }" if File.exists?(target_file)
+
+  unless File.exists?(target_file) || File.symlink?(target_file)
+    File.symlink(source_file, target_file)
+  end
+end
