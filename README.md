5b8af99256ff6f84d697ccf575cc1a730341a98d
kthibodeaux <keith@railscoder.net>
2014-06-29 11:34:23 -0400

    Add zsh

---
+++ README.md
@@ -0,0 +1,7 @@
+## dotfiles
+
+see .emacs.d/README.md for emacs related dependencies and information
+
+includes config for oh-my-zsh
+
+recommended to install [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh) before symlinking .zshrc
--- install.rb
+++ install.rb
@@ -1,17 +1,23 @@
 files = [
-  '.emacs.d',
-  '.gitconfig',
-  '.gitmessage',
-  '.rspec'
-]
+         '.emacs.d',
+         '.gitconfig',
+         '.gitmessage',
+         '.rspec',
+         '.zshrc'
+        ]
+
+def sym_it(source_file, target_file)
+  puts "skipping #{ target_file }" if File.exists?(target_file)
+
+  unless File.exists?(target_file) || File.symlink?(target_file)
+    puts "sym linking #{ target_file }"
+    File.symlink(source_file, target_file)
+  end
+end
 
 files.each do |file|
   target_file = File.expand_path("~/#{ file }")
   source_file = File.expand_path("~/.dotfiles/#{ file }")
 
-  puts "skipping #{ target_file }" if File.exists?(target_file)
-
-  unless File.exists?(target_file) || File.symlink?(target_file)
-    File.symlink(source_file, target_file)
-  end
+  sym_it(source_file, target_file)
 end
