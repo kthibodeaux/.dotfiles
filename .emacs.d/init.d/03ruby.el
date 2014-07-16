ac3648beb3e922969f57481d8451664e9978b8b1
Keith Thibodeaux <kthibodeaux@peachtreebilling.com>
2014-07-16 13:45:53 -0400

    Add method to allow natural typing of method names in def ... ()

--- .emacs.d/init.d/03ruby.el
+++ .emacs.d/init.d/03ruby.el
@@ -8,6 +8,25 @@
     (insert "{}")
     (backward-char 1)))
 
+(defun kdt-ruby-method-space-replace ()
+  "When pressing space while naming a defined method, insert an
+underscore"
+  (interactive)
+  (if (and (looking-back "def .+")
+	   (not
+	    (and
+	     (looking-at ".*)$")
+	     (looking-back "(.*")
+	     )
+	    )
+	   )
+	   (insert "_")
+	   (insert " ")
+	 ))
+
+
 (eval-after-load 'ruby-mode
   '(progn
-     (define-key ruby-mode-map (kbd "#") 'senny-ruby-interpolate)))
+     (define-key ruby-mode-map (kbd "#") 'senny-ruby-interpolate)
+     (define-key ruby-mode-map (kbd "SPC") 'kdt-ruby-method-space-replace)
+     ))
