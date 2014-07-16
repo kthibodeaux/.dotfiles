b44eeb690d42cb041dbe0d3767dd9f45e44f4014
kthibodeaux <keith@railscoder.net>
2014-07-16 18:44:42 -0400

    Update formatting to idiomatic elisp

--- .emacs.d/init.d/03ruby.el
+++ .emacs.d/init.d/03ruby.el
@@ -9,24 +9,16 @@
     (backward-char 1)))
 
 (defun kdt-ruby-method-space-replace ()
-  "When pressing space while naming a defined method, insert an
-underscore"
+  "When pressing space while naming a defined method, insert an underscore"
   (interactive)
   (if (and (looking-back "def .+")
-	   (not
-	    (and
-	     (looking-at ".*)$")
-	     (looking-back "(.*")
-	     )
-	    )
-	   )
-	   (insert "_")
-	   (insert " ")
-	 ))
-
+           (not (and
+                 (looking-at ".*)$")
+                 (looking-back "(.*"))))
+      (insert "_")
+    (insert " ")))
 
 (eval-after-load 'ruby-mode
   '(progn
      (define-key ruby-mode-map (kbd "#") 'senny-ruby-interpolate)
-     (define-key ruby-mode-map (kbd "SPC") 'kdt-ruby-method-space-replace)
-     ))
+     (define-key ruby-mode-map (kbd "SPC") 'kdt-ruby-method-space-replace)))
