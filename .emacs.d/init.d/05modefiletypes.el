fe7e6a88572dc38d391e4f8cadc1776a9722766a
Keith Thibodeaux <kthibodeaux@peachtreebilling.com>
2014-07-23 13:25:00 -0400

    Add Capfile and correct .Gemfile to Gemfile

--- .emacs.d/init.d/05modefiletypes.el
+++ .emacs.d/init.d/05modefiletypes.el
@@ -2,7 +2,8 @@
 (add-to-list 'auto-mode-alist '("\\.erubis$" . rhtml-mode))
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
