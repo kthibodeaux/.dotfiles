7961f1937b5561f39a1d29a55067233f1f941a01
Keith Thibodeaux <kthibodeaux@peachtreebilling.com>
2016-05-03 07:49:21 -0400

    Combine git alias and functions in one file

---
+++ .zsh/git.sh
@@ -0,0 +1,25 @@
+# vi: set ft=sh :
+
+# No arguments: `git status`
+# With arguments: acts like `git`
+g() {
+  if [[ $# > 0 ]]; then
+    git $@
+  else
+    git status
+  fi
+}
+
+# Complete g like git
+compdef g=git
+
+alias gp="git push"
+alias gb="git branch"
+alias gco="git checkout"
+alias gnew="git checkout -b"
+alias gcm="git commit -m"
+alias gd="git diff"
+alias gds="git diff --stat"
+alias gcp="git cherry-pick"
+
+alias churn="git log --all --name-only --format='format:' | grep -v '^$' | sort | uniq -c | sort -n | tail"
