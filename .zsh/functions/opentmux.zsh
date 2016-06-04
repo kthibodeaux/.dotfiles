1711ec8c878f6a83059b9dfa1e4dade916cd1bd3
kthibodeaux <keith@railscoder.net>
2016-06-04 10:15:54 -0400

    Extract opening tmux to function in own file
    This has a side effect of fixing the very first load of zshrc in iTerm
    where colors would not load.

---
+++ .zsh/functions/opentmux.zsh
@@ -0,0 +1,20 @@
+function opentmux() {
+  if [ "$TMUX" = "" ]; then
+    if [[ $(tmux ls) ]]; then
+      tmux ls
+      echo ""
+      echo "Enter a session number, press q to not launch tmux, or n for a new session"
+      read "response?[#/N/q] "
+      case $response in
+        [qQ])
+          return 1
+          ;;
+        *)
+          tmux attach -t $response || tmux new
+          ;;
+      esac
+    else
+      tmux new
+    fi
+  fi
+}
