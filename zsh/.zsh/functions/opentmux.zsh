bee2a8159020057213674d2ead5635590aa28b4e
Keith Thibodeaux <kthibodeaux@peachtreebilling.com>
2016-06-06 11:31:35 -0400

    Remove conditional for tmux ls and always give the prompt

--- zsh/.zsh/functions/opentmux.zsh
+++ zsh/.zsh/functions/opentmux.zsh
@@ -1,23 +1,16 @@
 function opentmux() {
   if [ "$TMUX" = "" ]; then
-    if [[ $(tmux ls) != 0 ]]; then
-      tmux ls
-      echo ""
-      echo "Enter a session number, press q to not launch tmux, or n for a new session"
-      read "response?[#/N/q] "
-      case $response in
-        [qQ])
-          return 1
-          ;;
-        *)
-          tmux attach -t $response || tmux new
-          ;;
-      esac
-    else
-      # Initial opening of iTerm2 will not load colors if tmux opens too quickly
-      # We sleep here to give colors time to load
-      # Since this is only ever going to execute if there are no other tmux sessions open it should be at most a once a day sleep
-      sleep 1; tmux new
-    fi
+    tmux ls
+    echo ""
+    echo "Enter a session number, press q to not launch tmux, or n for a new session"
+    read "response?[#/N/q] "
+    case $response in
+      [qQ])
+        return 1
+        ;;
+      *)
+        tmux attach -t $response || tmux new
+        ;;
+    esac
   fi
 }
