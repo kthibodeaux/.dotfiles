0265e95cc60b9ae453e00cb096e142757d6efeb7
Keith Thibodeaux <keith@railscoder.net>
2017-10-03 17:30:02 -0400

    zsh: `a` shows diff in fzf preview

--- zsh/.zsh/functions/git.zsh
+++ zsh/.zsh/functions/git.zsh
@@ -11,9 +11,9 @@ g() {
 
 a() {
   if [ "$TMUX" = "" ]; then
-    git add $(git status -s | awk '{ print $2 }' | fzf -m)
+    git add $(git status -s | awk '{ print $2 }' | fzf -m --preview 'git diff {}')
   else
-    git add $(git status -s | awk '{ print $2 }' | fzf-tmux -m)
+    git add $(git status -s | awk '{ print $2 }' | fzf-tmux -m --preview 'git diff {}')
   fi
 }
 
