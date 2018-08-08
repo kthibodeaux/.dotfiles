7d62d222ff2e455a54be738f2891e4a09c096ea7
Keith Thibodeaux <keith@railscoder.net>
2018-08-08 07:51:36 -0400

    setup: docker_compose setup extracted to own file

--- setup_steps/apt.sh
+++ setup_steps/apt.sh
@@ -79,6 +79,3 @@ sudo apt install \
   x11-xserver-utils \
   xclip \
   zsh
-
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
