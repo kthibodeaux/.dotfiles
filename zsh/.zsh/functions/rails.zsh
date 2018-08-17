a798a2284ee652ca1796aeefa58d349464bc1897
Keith Thibodeaux <keith@railscoder.net>
2018-08-17 07:13:32 -0400

    zsh: add `rq` and `rqbg` aliases for resque

--- zsh/.zsh/functions/rails.zsh
+++ zsh/.zsh/functions/rails.zsh
@@ -24,4 +24,7 @@ mi() { docker_or_local "bundle exec rake db:migrate RAILS_ENV=development" }
 mit() { docker_or_local "bundle exec rake db:migrate RAILS_ENV=test" }
 pmit() { docker_or_local "bundle exec rake parallel:migrate[$RSPEC_CORES]" }
 
+rq() { docker_or_local "bundle exec rake resque:work QUEUES=*" }
+rqbg() { docker_or_local "bundle exec rake resque:scheduler" }
+
 rl() { docker_or_local "bundle exec rake routes" | ag $1 }
