274d850c5cbcc9240499c170e9eaa35e39c11724
Keith Thibodeaux <keith@railscoder.net>
2019-06-27 05:44:35 -0400

    i3: auto configure external display if attached

---
+++ i3/.config/i3/monitors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+internal=$(xrandr -q | grep " connected" | cut -d" " -f1 | grep eDP)
+external=$(xrandr -q | grep " connected" | cut -d" " -f1 | grep -v eDP)
+
+
+if [ -z "$external" ]; then
+  exit 0
+else
+  xrandr --output $internal --mode 1920x1080 --left-of $external
+  xrandr --output $external --mode 2560x1440
+fi
