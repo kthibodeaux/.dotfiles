8e3abefd24c71d3805a07e57d2ba2712087529ca
Keith Thibodeaux <keith@railscoder.net>
2019-09-03 07:00:55 -0400

    zsh: remove unused docker function `db`

--- zsh/.zsh/functions/docker.zsh
+++ zsh/.zsh/functions/docker.zsh
@@ -1,10 +1,3 @@
-db() {
-  containers=$(docker ps -a --format "{{.ID}} {{.Names}} {{.Image}}" | grep unsafe_postgres)
-  target=$(echo $containers | $(fzf_prog) | awk '{ print $1 }')
-  echo $containers | awk '{ print $1 }' | xargs -I NAME docker stop NAME > /dev/null
-  docker start $target > /dev/null
-}
-
 docker_or_local() {
   local image=""
   local default_image="web"
