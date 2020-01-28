661249c3849131eac37a8e99142820ca5d29c22c
Keith Thibodeaux <keith@railscoder.net>
2020-01-28 06:43:44 -0500

    zsh: consolidate location of wf related things

--- zsh/.zsh/functions/wf.zsh
+++ zsh/.zsh/functions/wf.zsh
@@ -1,3 +1,7 @@
+next_story() {
+  wf --start-card --card $(wf --list-unstarted-cards | fzf-tmux | awk '{print $1;}') --include-related
+}
+
 rft() {
   if [[ $# > 0 ]]; then
     wf --mark-ready-for-testing --list $1
