9f15316fa0fcfe52f568a1e006497360af65d3f0
Keith Thibodeaux <keith@railscoder.net>
2020-02-21 17:51:37 -0500

    i3: do not care about internal monitors
    no longer using a laptop as a main station with an external

--- i3/.config/i3/monitors.sh
+++ i3/.config/i3/monitors.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-internal=$(xrandr -q | grep " connected" | cut -d" " -f1 | grep eDP)
-external=$(xrandr -q | grep " connected" | cut -d" " -f1 | grep -v eDP)
-
-
-if [ -z "$external" ]; then
-  exit 0
-else
-  xrandr --output $internal --mode 1920x1080 --left-of $external
-  xrandr --output $external --mode 2560x1440
-fi
 
 feh --bg-scale /home/kthibodeaux/Pictures/background.jpg
