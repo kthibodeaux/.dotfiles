516afdbd21c8c85049be8efc356028c4b0e317a8
Keith Thibodeaux <keith@railscoder.net>
2020-03-18 05:44:17 -0400

    i3: release management script is way smarter

--- i3/.config/i3/scripts/trello_release_progress.rb
+++ i3/.config/i3/scripts/trello_release_progress.rb
@@ -2,28 +2,30 @@
 require 'json'
 require 'yaml'
 
-DEV_BOARD_ID = 'iUU2lzsz'.freeze
+RELEASE_MANAGEMENT_BOARD_ID = 'B90ZfDvh'.freeze
 TRELLO = YAML.load(File.open("#{ ENV['HOME'] }/.wf.yml", 'r')).freeze
 AUTH = "key=#{TRELLO['trello']['key']}&token=#{TRELLO['trello']['token']}".freeze
-READY_LABELS = [
-  'Accepted',
-  'Fixed in Production',
-  'Hotfixed',
-  'No Testing Required',
-].freeze
 
-all_lists = JSON.parse(`curl --silent 'https://api.trello.com/1/boards/#{DEV_BOARD_ID}/?fields=name&lists=all&list_fields=all&#{AUTH}'`)
-active_lists = all_lists.fetch('lists').reject { |e| e.fetch('closed') }
+LIST_IDS = {
+  on_deck: '58cc45de4041c3eeacedc85f',
+  ready_for_testing: '5878ea48e1b7168900bfbb3a',
+  testing_in_progress: '588a22d8615830d94cf7ad98',
+  ready_for_release: '588a22e48e9dae28ecbc95d6',
+}.freeze
 
-current_release_list_position = active_lists.map { |e| e.fetch('name') }.index('Pull Request') + 1
-current_release_list_id = active_lists[current_release_list_position].fetch('id')
-current_release_list_name = active_lists[current_release_list_position].fetch('name')
 
-cards = JSON.parse(`curl --silent 'https://api.trello.com/1/lists/#{current_release_list_id}/cards?#{AUTH}'`)
-
-ready_cards = cards.select do |card|
-  labels = card.fetch('labels').map { |e| e.fetch('name') }
-  labels.any? { |label| READY_LABELS.include?(label) }
+cards = {}.tap do |c|
+  LIST_IDS.map do |key, id|
+    c[key] = JSON.parse(`curl --silent 'https://api.trello.com/1/lists/#{id}/cards?#{AUTH}'`)
+  end
 end
 
-puts "#{current_release_list_name}: #{ready_cards.size}/#{cards.size}"
+if cards.fetch(:ready_for_release).any?
+  puts "Ready for Release: #{cards.fetch(:ready_for_release).first.fetch('name').partition(' ').first}"
+elsif cards.fetch(:testing_in_progress).any?
+  puts "In Testing: #{cards.fetch(:testing_in_progress).first.fetch('name').partition(' ').first}"
+elsif cards.fetch(:ready_for_testing).any?
+  puts "Ready for Testing: #{cards.fetch(:ready_for_testing).first.fetch('name').partition(' ').first}"
+elsif cards.fetch(:on_deck).any?
+  puts "Next: #{cards.fetch(:on_deck).first.fetch('name').partition(' ').first}"
+end
