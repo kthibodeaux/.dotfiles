c5b843b12316ea91f538021f179ad878364ebaa4
Keith Thibodeaux <keith@railscoder.net>
2020-09-28 17:44:25 -0400

    i3: correctly configure screens when on laptop

--- i3/.config/i3/monitors.sh
+++ i3/.config/i3/monitors.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+hasexternal=$(xrandr -q | grep " connected" | cut -d" " -f1 | grep HDMI-1-1)
+
+if [ -z "$hasexternal" ]; then
+  # on single monitor desktop
+  exit 0
+else
+  # on laptop with lid closed
+  xrandr --output "eDP1" --off
+  xrandr --output "HDMI-1-1" --auto
+fi
+
 feh --bg-scale /home/kthibodeaux/Pictures/background.jpg
