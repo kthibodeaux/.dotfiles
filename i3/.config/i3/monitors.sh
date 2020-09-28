e57894f692f3affa20dd7a8d8bb1b2813302e430
Keith Thibodeaux <keith@railscoder.net>
2020-09-28 18:25:05 -0400

    i3: determine monitor names dynamically

--- i3/.config/i3/monitors.sh
+++ i3/.config/i3/monitors.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-hasexternal=$(xrandr -q | grep " connected" | cut -d" " -f1 | grep HDMI-1-1)
+internal=$(xrandr -q | grep " connected" | cut -d" " -f1 | grep eDP)
+external=$(xrandr -q | grep " connected" | cut -d" " -f1 | grep HDMI)
 
-if [ -z "$hasexternal" ]; then
+if [ -z "$external" ]; then
   # on single monitor desktop
   exit 0
 else
   # on laptop with lid closed
-  xrandr --output "eDP1" --off
-  xrandr --output "HDMI-1-1" --auto
+  xrandr --output "$internal" --off
+  xrandr --output "$external" --auto
 fi
 
 feh --bg-scale /home/kthibodeaux/Pictures/background.jpg
