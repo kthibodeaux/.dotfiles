608fb0c1b73a3f265a743a533bfc577cbcdfbf27
Keith Thibodeaux <keith@railscoder.net>
2021-07-13 09:18:57 -0400

    nvim: hjkl remap works across modes

--- neovim/.config/nvim/lua/keyboard_layouts.lua
+++ neovim/.config/nvim/lua/keyboard_layouts.lua
@@ -2,9 +2,9 @@ local options = { noremap = true }
 local silent_options = { noremap = true, silent = true }
 
 local function use_qwerty()
-  vim.api.nvim_set_keymap('n', 'h', 'h', options)
-  vim.api.nvim_set_keymap('n', 'j', 'j', options)
-  vim.api.nvim_set_keymap('n', 'k', 'k', options)
+  vim.api.nvim_set_keymap('', 'h', 'h', options)
+  vim.api.nvim_set_keymap('', 'j', 'j', options)
+  vim.api.nvim_set_keymap('', 'k', 'k', options)
 
   vim.api.nvim_set_keymap('n', '<C-h>', ':TmuxNavigateLeft<CR>', options)
   vim.api.nvim_set_keymap('n', '<C-j>', ':TmuxNavigateDown<CR>', options)
@@ -15,9 +15,9 @@ local function use_qwerty()
 end
 
 local function use_colemak()
-  vim.api.nvim_set_keymap('n', 'h', 'k', options)
-  vim.api.nvim_set_keymap('n', 'j', 'h', options)
-  vim.api.nvim_set_keymap('n', 'k', 'j', options)
+  vim.api.nvim_set_keymap('', 'h', 'k', options)
+  vim.api.nvim_set_keymap('', 'j', 'h', options)
+  vim.api.nvim_set_keymap('', 'k', 'j', options)
 
   vim.api.nvim_set_keymap('n', '<C-j>', ':TmuxNavigateLeft<CR>', options)
   vim.api.nvim_set_keymap('n', '<C-k>', ':TmuxNavigateDown<CR>', options)
