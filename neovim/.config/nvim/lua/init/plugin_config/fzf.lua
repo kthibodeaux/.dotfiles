c4ef45874c0369fb5b88e671b4451a8916cef1fc
Keith Thibodeaux <keith@railscoder.net>
2022-03-30 08:10:43 -0400

    ag: add .agignore to globally ignore .git/

--- neovim/.config/nvim/lua/init/plugin_config/fzf.lua
+++ neovim/.config/nvim/lua/init/plugin_config/fzf.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_exec([[
-command! -bang -nargs=* Find call fzf#vim#grep('ag --hidden -A 0 -B 0 --ignore .git --nogroup --color '.shellescape(<q-args>), 0, fzf#vim#with_preview('up:70%'))
+command! -bang -nargs=* Find call fzf#vim#grep('ag --hidden -A 0 -B 0 --nogroup --color '.shellescape(<q-args>), 0, fzf#vim#with_preview('up:70%'))
 
 nnoremap <leader>e :Files<cr>
 
