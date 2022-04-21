8eafa4bf07d27268c56228daf9973c5f756a5ac2
Keith Thibodeaux <keith@railscoder.net>
2022-04-21 07:50:42 -0400

    zsh: use new deploy command

--- zsh/.zsh/functions/wf.zsh
+++ zsh/.zsh/functions/wf.zsh
@@ -2,10 +2,9 @@ next_story() {
   wf --start-card --card $(wf --list-unstarted-cards | fzf-tmux | awk '{print $1;}') --include-related
 }
 
-rft() {
+deploy() {
   if [[ $# > 0 ]]; then
-    wf --mark-ready-for-testing --list $1
-    wf --prepare-release-card --list $1
+    wf --deploy --list $1
   else
     echo "argument required (release name)"
   fi
