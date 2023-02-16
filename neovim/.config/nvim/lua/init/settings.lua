f3f2cef0d34980585057769464512600688c8d2f
Keith Thibodeaux <keith@railscoder.net>
2023-02-16 09:31:16 -0500

    vim: always show signcolumn
    if a file has no linting errors, but then a linting error is introduced,
    the signcolumn will appear to show the error icon and that shifts all
    the text over. by leaving the sign column open it can never shift the
    text

--- neovim/.config/nvim/lua/init/settings.lua
+++ neovim/.config/nvim/lua/init/settings.lua
@@ -6,6 +6,7 @@ vim.g.maplocalleader = ','
 vim.g.netrw_liststyle = 3
 vim.g.netrw_bufsettings = 'noma nomod nu nobl nowrap ro'
 
+vim.opt.signcolumn = 'yes'
 vim.opt.autoindent = true
 vim.opt.backspace = 'indent,start,eol'
 vim.opt.backupcopy = 'yes'
