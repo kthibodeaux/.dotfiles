4d09407b8b2e85df9b9bdb9e58e22337c82fe890
Keith Thibodeaux <keith@railscoder.net>
2023-08-31 09:02:45 -0400

    i3: github notifications open in firefox on click

--- i3/.config/i3/scripts/github_notifications.rb
+++ i3/.config/i3/scripts/github_notifications.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env /home/kthibodeaux/.rubies/ruby-3.1.1/bin/ruby
 require 'json'
 
+`firefox https://github.com/notifications?query=is%3Aunread` if ENV['BLOCK_BUTTON'].to_i == 1
+
 TOKEN = File.readlines("#{ENV['HOME']}/.github_token").first.chomp
 
 notifications = JSON.parse(`curl --silent -H "Authorization: token #{TOKEN}" "https://api.github.com/notifications"`)
