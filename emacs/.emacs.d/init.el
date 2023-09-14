8906c91ef5eb1631d6b8c682528f7497e15f35df
Keith Thibodeaux <keith@railscoder.net>
2023-09-14 04:48:22 -0400

    emacs: setup package framework

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -10,3 +10,17 @@
 
 ; make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+; packages
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
