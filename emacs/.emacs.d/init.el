8170874b4241f071714ae4caeae7f11ff1d88387
Keith Thibodeaux <keith@railscoder.net>
2023-09-14 11:36:34 -0400

    emacs: add helpful

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -70,3 +70,14 @@
 (use-package counsel
   :config
   (counsel-mode 1))
+
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
