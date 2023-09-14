6310fe55a5b4a822c6c22eb081a4e30615ec2e5f
Keith Thibodeaux <keith@railscoder.net>
2023-09-14 04:48:42 -0400

    emacs: use ivy

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -24,3 +24,22 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
+
+; use ivy for completion
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
