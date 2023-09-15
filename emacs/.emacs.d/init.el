f665252b0ff6dff014bb5ae8450b9a00fe69b70b
Keith Thibodeaux <keith@railscoder.net>
2023-09-15 05:44:26 -0400

    emacs: configure backup and autosave file behavior

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -159,3 +159,20 @@
 
 (setq custom-file (locate-user-emacs-file "~/.emacs-custom"))
 (load custom-file 'noerror 'nomessage)
+
+(let ((backup-dir "~/emacs.d/backups")
+      (auto-saves-dir "~/emacs.d/autosaves/"))
+  (dolist (dir (list backup-dir auto-saves-dir))
+    (when (not (file-directory-p dir))
+      (make-directory dir t)))
+  (setq backup-directory-alist `(("." . ,backup-dir))
+        auto-save-file-name-transforms `((".*" ,auto-saves-dir t))
+        auto-save-list-file-prefix (concat auto-saves-dir ".saves-")
+        tramp-backup-directory-alist `((".*" . ,backup-dir))
+        tramp-auto-save-directory auto-saves-dir))
+(setq
+   backup-by-copying t
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)
