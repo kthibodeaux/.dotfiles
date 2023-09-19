ba7a1836d6cbfae40486fc22558261cbe835ebd7
Keith Thibodeaux <keith@railscoder.net>
2023-09-19 19:02:19 -0400

    vim: fix FixRailsDBStructure command

--- neovim/.config/nvim/lua/init/commands.lua
+++ neovim/.config/nvim/lua/init/commands.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_exec([[
-command! -nargs=0 FixRailsDBStructure :normal G?HEAD<CR>maG?><CR>V`a:s/;/,/g<CR>`akGVG?>><CR>:g!/^[(]/d<CR>`addVG?,<CR>:sort u<CR>G?,<CR>r;:w<CR>
+command! -nargs=0 FixRailsDBStructure :normal G?HEAD<CR>maG?><CR>V`a:s/;/,/g<CR>`ajGVG?>><CR>:g!/^[(]/d<CR>`addVG?,<CR>:sort u<CR>G?,<CR>r;:wq<CR>
 ]], false)
