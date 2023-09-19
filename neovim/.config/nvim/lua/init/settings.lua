4a45222ec72059e8033f466a1ba682feb4e4323f
Keith Thibodeaux <keith@railscoder.net>
2023-09-19 06:10:18 -0400

    vim: extract crosshairs to own file

--- neovim/.config/nvim/lua/init/settings.lua
+++ neovim/.config/nvim/lua/init/settings.lua
@@ -50,22 +50,6 @@ vim.opt.winwidth = 90
 vim.opt.wrap = false
 vim.opt.mouse=
 
-vim.api.nvim_exec([[
-" Highlight current line in active pane only
-augroup CursorLine
-  au!
-  au VimEnter,WinEnter,BufWinEnter * setlocal cursorline
-  au WinLeave * setlocal nocursorline
-augroup END
-
-" Highlight current column in active pane only
-augroup CursorColumn
-  au!
-  au VimEnter,WinEnter,BufWinEnter * setlocal cursorcolumn
-  au WinLeave * setlocal nocursorcolumn
-augroup END
-]], false)
-
 require('nightfox').setup({
   options = {
     styles = {
