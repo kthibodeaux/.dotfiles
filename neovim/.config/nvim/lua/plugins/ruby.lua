b7b689f79ba141122de0b5dce7ff46daac94a03f
Keith Thibodeaux <keith@railscoder.net>
2023-09-20 14:41:16 -0400

    vim: add ruby plugins via lazy

---
+++ neovim/.config/nvim/lua/plugins/ruby.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'thoughtbot/vim-rspec',
+  },
+  {
+    'tpope/vim-endwise'
+  },
+  {
+    'tpope/vim-rails'
+  },
+  {
+    'rhysd/vim-textobj-ruby',
+    dependencies = {
+      'kana/vim-textobj-user'
+    },
+  },
+}
