439169d86787c3436c923e03e9afbe31e364eb56
Keith Thibodeaux <keith@railscoder.net>
2023-09-20 16:55:32 -0400

    vim: better indentation

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -6,27 +6,27 @@ require('init/settings')
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 local fs_stat
 if vim.fn.has('nvim-0.10.0') == 1 then
-    fs_stat = vim.uv.fs_stat
+  fs_stat = vim.uv.fs_stat
 else
-    -- Deprecated in 0.10+
-    fs_stat = vim.loop.fs_stat
+  -- Deprecated in 0.10+
+  fs_stat = vim.loop.fs_stat
 end
 
 if not fs_stat(lazypath) then
-    vim.notify('Installing lazy...')
+  vim.notify('Installing lazy...')
 
-    local result = vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable',
-        lazypath,
-    })
+  local result = vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
 
-    if vim.v.shell_error ~= 0 then
-        print('ERROR: ' .. result)
-    end
+  if vim.v.shell_error ~= 0 then
+    print('ERROR: ' .. result)
+  end
 end
 
 vim.opt.runtimepath:prepend(lazypath)
