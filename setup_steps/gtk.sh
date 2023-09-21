1e77b0ec0416b34d8bb80336571cc5ebd60c4bdf
Keith Thibodeaux <keith@railscoder.net>
2023-09-21 17:25:49 -0400

    gtk/rofi: use dracula theme

--- setup_steps/gtk.sh
+++
@@ -1,9 +0,0 @@
-if [ -d ~/src/numix-solarized-gtk-theme ]; then
-  echo "skipping install gtk theme: already installed"
-else
-  cd ~/src
-  git clone git@github.com:Ferdi265/numix-solarized-gtk-theme.git
-  cd numix-solarized-gtk-theme
-  sudo make THEME=SolarizedDarkGreen install
-  cd ~
-fi
