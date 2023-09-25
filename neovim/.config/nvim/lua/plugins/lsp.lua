da6dc08051268943507ed713403edd71b3ef5b63
Keith Thibodeaux <keith@railscoder.net>
2023-09-25 03:13:44 -0400

    vim: better cmp mappings
    stolen from https://github.com/nvim-lua/kickstart.nvim

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -65,6 +65,7 @@ return {
     },
     config = function()
       local cmp = require("cmp")
+      local luasnip = require 'luasnip'
       local cmp_action = require('lsp-zero').cmp_action()
       vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
@@ -90,7 +91,29 @@ return {
         mapping = cmp.mapping.preset.insert({
           ['<Tab>'] = cmp_action.luasnip_supertab(),
           ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<C-Space>'] = cmp.mapping.complete {},
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+          },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
         })
       })
 
