3edaafc90939ef7cc2b0d0201863b90a7ac08cc5
Keith Thibodeaux <keith@railscoder.net>
2023-09-26 07:47:10 -0400

    vim: always load oil
    OpenDbMigrate depends on it, and if it hasnt opened yet :Oil will not be
    a valid command

--- neovim/.config/nvim/lua/plugins/files.lua
+++ neovim/.config/nvim/lua/plugins/files.lua
@@ -8,6 +8,7 @@ return {
   {
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
     opts = {},
     keys = {
       { '-', "<cmd>Oil<CR>", desc = 'open parent dir' },
