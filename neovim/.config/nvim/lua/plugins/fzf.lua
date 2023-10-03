8e2567d48541bbe298c12809fb77bdd5397d7725
Keith Thibodeaux <keith@railscoder.net>
2023-10-03 13:33:49 -0400

    vim: grepping does not escape characters
    this enables the initial query to use regex

--- neovim/.config/nvim/lua/plugins/fzf.lua
+++ neovim/.config/nvim/lua/plugins/fzf.lua
@@ -15,7 +15,7 @@ return {
   end,
   keys = {
     { '<leader>e', "<cmd>lua require('fzf-lua').files()<CR>", desc = 'find file' },
-    { '<leader>ff', "<cmd>lua require('fzf-lua').grep()<CR>", desc = 'grep' },
+    { '<leader>ff', "<cmd>lua require('fzf-lua').grep({ no_esc=true })<CR>", desc = 'grep' },
     { '<leader>fa', "<cmd>lua require('fzf-lua').grep_cword()<CR>", desc = 'grep current word' },
   },
 }
