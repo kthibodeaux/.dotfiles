16b1f21f097f8d2e5130d84ece106a9f01193309
Keith Thibodeaux <keith@railscoder.net>
2023-10-03 05:34:46 -0400

    vim: ensure bash treesitter grammar is installed

--- neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -15,6 +15,7 @@ return {
     configs.setup({
       -- A list of parser names, or 'all' (the five listed parsers should always be installed)
       ensure_installed = {
+        'bash',
         'diff',
         'git_rebase',
         'javascript',
