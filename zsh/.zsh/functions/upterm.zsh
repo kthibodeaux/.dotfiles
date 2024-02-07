8c3b5275746327209f846364b6604605a91c5b24
Keith Thibodeaux <keith@railscoder.net>
2024-02-07 17:10:02 -0500

    zsh: add `pair` to copy upterm ssh address
    must be run in a new window if pairing to an existing session

--- zsh/.zsh/functions/upterm.zsh
+++ zsh/.zsh/functions/upterm.zsh
@@ -2,3 +2,8 @@ choose_pairs() {
   names=($(cat ~/pairs.txt | $(fzf_prog) -m))
   printf "--github-user %s " "${names[@]}"
 }
+
+pair() {
+  upterm session current | grep 'SSH Session' | tr -s ' ' | cut -d' ' -f3,4 | xclip -selection clipboard
+  echo "Copied upterm SSH address to clipboard"
+}
