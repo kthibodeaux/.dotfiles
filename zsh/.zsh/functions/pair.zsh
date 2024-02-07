bc2c36492f2eed8ef5452a163e4b4ca3a8b4cd70
Keith Thibodeaux <keith@railscoder.net>
2024-02-07 14:50:51 -0500

    zsh: pair accepts github username as arg

--- zsh/.zsh/functions/pair.zsh
+++ zsh/.zsh/functions/pair.zsh
@@ -1,6 +1,6 @@
 pair() {
   if [ "$TMUX" = "" ]; then
-    upterm host --force-command 'tmux attach -t pair-programming' --github-user scottleeroberts --github-user mylesboone --github-user dms449 -- tmux new -t pair-programming
+    upterm host --force-command 'tmux attach -t pair-programming' --github-user $1
   else
     upterm session current | grep 'SSH Session' | tr -s ' ' | cut -d' ' -f3,4 | xclip -selection clipboard
     echo "Copied address to clipboard"
