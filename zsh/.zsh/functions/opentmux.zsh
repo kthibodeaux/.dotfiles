097e31c7241070fbf103e24c77ee4e1aa60c070d
Keith Thibodeaux <keith@railscoder.net>
2024-02-07 15:26:03 -0500

    zsh: tmux prompt allows creating new pair session

--- zsh/.zsh/functions/opentmux.zsh
+++ zsh/.zsh/functions/opentmux.zsh
@@ -1,12 +1,20 @@
+choose_pairs() {
+  names=($(cat ~/pairs.txt | $(fzf_prog) -m))
+  printf "--github-user %s " "${names[@]}"
+}
+
 function opentmux() {
   if [ "$TMUX" = "" ]; then
     tmux ls
     echo ""
-    echo "Enter a session number, press q to not launch tmux, or n for a new session"
-    read "response?[#/N/q] "
+    echo "Enter a session number, p for new pair session, q to not launch tmux, n for a new session"
+    read "response?[#/p/n/q] "
     case $response in
       [qQ])
         return 1
+        ;;
+      [pP])
+        upterm host --force-command 'tmux attach -t pair-programming' $(choose_pairs) -- tmux new -t pair-programming
         ;;
       *)
         tmux attach -t $response || tmux new
