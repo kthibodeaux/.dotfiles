609cb76af8d650ef6d18026660615eb7e8dda508
Keith Thibodeaux <keith@railscoder.net>
2024-02-07 15:25:52 -0500

    zsh: remove old pair function

--- zsh/.zsh/functions/pair.zsh
+++
@@ -1,8 +0,0 @@
-pair() {
-  if [ "$TMUX" = "" ]; then
-    upterm host --force-command 'tmux attach -t pair-programming' --github-user $1
-  else
-    upterm session current | grep 'SSH Session' | tr -s ' ' | cut -d' ' -f3,4 | xclip -selection clipboard
-    echo "Copied address to clipboard"
-  fi
-}
