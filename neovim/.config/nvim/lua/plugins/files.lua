28563dc59c46170b2d42c44f7a292eb9c253e240
Keith Thibodeaux <keith@railscoder.net>
2024-02-11 19:11:19 -0500

    nvim: oil shows hidden files by default

--- neovim/.config/nvim/lua/plugins/files.lua
+++ neovim/.config/nvim/lua/plugins/files.lua
@@ -8,6 +8,13 @@ return {
   {
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require('oil').setup({
+        view_options = {
+          show_hidden = true,
+        },
+      })
+    end,
     lazy = false,
     opts = {},
     keys = {
