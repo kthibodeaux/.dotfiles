20e1f70d51afdd93cc556d3acec2b77dc5665d39
Keith Thibodeaux <keith@railscoder.net>
2024-03-15 18:05:16 -0400

    vim: allow C-l and C-h to navigate splits when inside oil

--- neovim/.config/nvim/lua/plugins/files.lua
+++ neovim/.config/nvim/lua/plugins/files.lua
@@ -13,6 +13,10 @@ return {
         view_options = {
           show_hidden = true,
         },
+        keymaps = {
+          ["<C-l>"] = false,
+          ["<C-h>"] = false,
+        },
       })
     end,
     lazy = false,
