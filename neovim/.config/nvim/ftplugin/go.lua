f9baa2f0ef5045d15f2d3272db505ea3ddeaa0ef
Keith Thibodeaux <keith@railscoder.net>
2024-03-28 10:14:02 -0400

    vim: refactor tests

--- neovim/.config/nvim/ftplugin/go.lua
+++ neovim/.config/nvim/ftplugin/go.lua
@@ -7,4 +7,4 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.copyindent = true
 vim.opt_local.preserveindent = true
 
-buffer_map(0, 'n', '<leader>ra', ':lua _G.tests.go_run_all()<CR>', { noremap = true, desc = 'all files' })
+buffer_map(0, 'n', '<leader>ra', ':lua _G.tests.go.run_all()<CR>', { noremap = true, desc = 'all files' })
--- neovim/.config/nvim/lua/tests.lua
+++ neovim/.config/nvim/lua/tests.lua
@@ -1,31 +1,31 @@
-local function go_run_all()
-  vim.cmd("VimuxRunCommand('go test ./...')")
-end
+local go = {
+  run_all = function()
+    vim.cmd("VimuxRunCommand('go test ./...')")
+  end,
+}
 
-local function rspec_run_all()
-  vim.cmd("VimuxRunCommand('bundle exec rspec')")
-end
+local rspec = {
+  run_all = function()
+    vim.cmd("VimuxRunCommand('bundle exec rspec')")
+  end,
+  run_file = function()
+    local filename = vim.fn.expand('%')
+    vim.cmd('VimuxRunCommand("bundle exec rspec ' .. filename .. '")')
+  end,
+  run_nearest = function()
+    local filename = vim.fn.expand('%')
+    local line, column = unpack(vim.api.nvim_win_get_cursor(0))
 
-local function rspec_run_file()
-  local filename = vim.fn.expand('%')
-  vim.cmd('VimuxRunCommand("bundle exec rspec ' .. filename .. '")')
-end
-
-local function rspec_run_nearest()
-  local filename = vim.fn.expand('%')
-  local line, column = unpack(vim.api.nvim_win_get_cursor(0))
-
-  vim.cmd('VimuxRunCommand("bundle exec rspec ' .. filename .. ':' .. line .. '")')
-end
+    vim.cmd('VimuxRunCommand("bundle exec rspec ' .. filename .. ':' .. line .. '")')
+  end,
+}
 
 local function run_last()
   vim.cmd("VimuxRunLastCommand")
 end
 
 return {
-  go_run_all = go_run_all,
-  rspec_run_all = rspec_run_all,
-  rspec_run_file = rspec_run_file,
-  rspec_run_nearest = rspec_run_nearest,
+  go = go,
+  rspec = rspec,
   run_last = run_last,
 }
