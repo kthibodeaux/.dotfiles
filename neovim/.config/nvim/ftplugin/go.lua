8807ccee870048985c9d8e99fda33bf0130c4076
Keith Thibodeaux <keith@railscoder.net>
2024-03-28 08:48:46 -0400

    vim: add go test map

--- neovim/.config/nvim/ftplugin/go.lua
+++ neovim/.config/nvim/ftplugin/go.lua
@@ -1,6 +1,10 @@
+local buffer_map = vim.api.nvim_buf_set_keymap
+
 vim.opt_local.expandtab = false
 vim.opt_local.softtabstop = 0
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.copyindent = true
 vim.opt_local.preserveindent = true
+
+buffer_map(0, 'n', '<leader>ra', ':call VimuxRunCommand("go test ./...")<CR>', { noremap = true, desc = 'all files' })
