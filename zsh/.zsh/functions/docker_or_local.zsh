8a303cc1fd59c75828285ad99f19742f591661ea
Keith Thibodeaux <keith@railscoder.net>
2024-03-28 09:15:46 -0400

    zsh: bundle is docker_or_local and all old functions are now aliases

--- zsh/.zsh/functions/docker_or_local.zsh
+++ zsh/.zsh/functions/docker_or_local.zsh
@@ -1,22 +1,4 @@
+bundle() { docker_or_local "bundle $@" }
 go() { docker_or_local "go $@" }
-
 vue() { docker_or_local "vue $@" }
-ya() { docker_or_local "yarn $@" }
-
-be() { docker_or_local "bundle exec $@" }
-bu() { docker_or_local "bundle update $@" }
-bi() { docker_or_local "bundle install $@" }
-
-clean_devdb() { docker_or_local "bundle exec rake db:drop db:setup RAILS_ENV=development" }
-clean_testdb() { docker_or_local "bundle exec rake db:drop db:setup RAILS_ENV=test" }
-pclean_testdb() { docker_or_local "bundle exec rake parallel:drop[$RSPEC_CORES] parallel:setup[$RSPEC_CORES] RAILS_ENV=test" }
-
-ber() { docker_or_local "bundle exec rails $@" }
-bes() { docker_or_local "bundle exec rspec $@" }
-bep() { docker_or_local "bundle exec rake parallel:spec[$RSPEC_CORES] RAILS_ENV=test" }
-
-mi() { docker_or_local "bundle exec rake db:migrate RAILS_ENV=development" }
-mit() { docker_or_local "bundle exec rake db:migrate RAILS_ENV=test" }
-pmit() { docker_or_local "bundle exec rake parallel:migrate[$RSPEC_CORES]" }
-
-rl() { docker_or_local "bundle exec rails routes" | ag $1 }
+yarn() { docker_or_local "yarn $@" }
