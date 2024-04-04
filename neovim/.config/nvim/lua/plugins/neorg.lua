9a19db595a2712dd461cd1c285c487da1d19b3bc
Keith Thibodeaux <kthibodeaux@railscoder.net>
2024-04-04 17:45:53 -0400

    vim: update neorg

--- neovim/.config/nvim/lua/plugins/neorg.lua
+++ neovim/.config/nvim/lua/plugins/neorg.lua
@@ -1,30 +1,36 @@
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("neorg").setup {
-      load = {
-        ["core.defaults"] = {},
-        ["core.completion"] = {
-          config = {
-            engine = "nvim-cmp",
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            },
           },
-        },
-        ["core.concealer"] = {
-          config = {
-            icon_preset = "diamond",
+          ["core.concealer"] = {
+            config = {
+              icon_preset = "diamond",
+            },
           },
-        },
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              notes = "~/notes",
-              yonder = "~/tabletop_rpg/Dungeons and Dragons 5e/My Campaigns/Yonder/neorg",
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+                yonder = "~/tabletop_rpg/Dungeons and Dragons 5e/My Campaigns/Yonder/neorg",
+              },
             },
           },
         },
-      },
-    }
-  end,
+      }
+    end,
+  }
 }
