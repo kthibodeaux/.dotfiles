4eaf1135e051f6724b590ff4a2579b5e0bd3089e
Keith Thibodeaux <keith@railscoder.net>
2024-08-12 06:44:41 -0400

    remove upterm

--- zsh/.zsh/functions/opentmux.zsh
+++ zsh/.zsh/functions/opentmux.zsh
@@ -2,20 +2,11 @@ function opentmux() {
   if [ "$TMUX" = "" ]; then
     tmux ls
     echo ""
-    echo "Enter a session number, p for new pair session, q to not launch tmux, n for a new session"
-    read "response?[#/p/n/q] "
+    echo "Enter a session number, q to not launch tmux, n for a new session"
+    read "response?[#/n/q] "
     case $response in
       [qQ])
         return 1
-        ;;
-      [pP])
-        vared -p "Enter a session number, n for a new session " -c session_name
-
-        if [ "$session_name" = "n" ]; then
-          upterm host --force-command 'tmux attach -t pair-programming' $(choose_pairs) -- tmux new -t pair-programming
-        else
-          upterm host --force-command "tmux attach -t $session_name" $(choose_pairs) -- tmux attach -t $session_name
-        fi
         ;;
       *)
         tmux attach -t $response || tmux new
--- zsh/.zsh/functions/upterm.zsh
+++
@@ -1,9 +0,0 @@
-choose_pairs() {
-  names=($(cat ~/pairs.txt | fzf -m))
-  printf "--github-user %s " "${names[@]}"
-}
-
-pair() {
-  upterm session current | grep 'SSH Session' | tr -s ' ' | cut -d' ' -f3,4 | xclip -selection clipboard
-  echo "Copied upterm SSH address to clipboard"
-}
