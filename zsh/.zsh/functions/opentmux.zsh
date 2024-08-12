4ec11cdc5af1f259a6e77f99ca183a1309750c57
Keith Thibodeaux <keith@railscoder.net>
2024-08-12 06:58:41 -0400

    zsh: tmux pair launches tty-share

--- zsh/.zsh/functions/opentmux.zsh
+++ zsh/.zsh/functions/opentmux.zsh
@@ -2,11 +2,14 @@ function opentmux() {
   if [ "$TMUX" = "" ]; then
     tmux ls
     echo ""
-    echo "Enter a session number, q to not launch tmux, n for a new session"
-    read "response?[#/n/q] "
+    echo "Enter a session number, p for new pair session, q to not launch tmux, n for a new session"
+    read "response?[#/p/n/q] "
     case $response in
       [qQ])
         return 1
+        ;;
+      [pP])
+        tty-share -public -listen "localhost:8010"
         ;;
       *)
         tmux attach -t $response || tmux new
