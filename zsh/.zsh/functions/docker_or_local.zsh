2c508a93fe79342f53306acf713d6a0b5731ecc6
Keith Thibodeaux <keith@railscoder.net>
2024-09-19 10:10:25 -0400

    zsh: python uses docker_or_local

--- zsh/.zsh/functions/docker_or_local.zsh
+++ zsh/.zsh/functions/docker_or_local.zsh
@@ -1,4 +1,5 @@
 bundle() { docker_or_local "bundle $@" }
 go() { docker_or_local "go $@" }
+python() { docker_or_local "python $@" }
 vue() { docker_or_local "vue $@" }
 yarn() { docker_or_local "yarn $@" }
