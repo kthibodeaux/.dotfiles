51c2107ea429ac3c2e2c6158d660a5c6bbccaaac
Keith Thibodeaux <keith@railscoder.net>
2025-04-15 07:39:28 -0400

    vim: use blink for completion and snippets

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -1,9 +1,17 @@
 return {
   {
-    'echasnovski/mini.completion',
-    version = false,
-    config = function()
-      require('mini.completion').setup()
-    end,
+    'saghen/blink.cmp',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    version = '1.*',
+    opts = {
+      keymap = { preset = 'enter' },
+      completion = { documentation = { auto_show = true } },
+      sources = {
+        default = { 'snippets', 'lsp', 'path', 'buffer' },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+      signature = { enabled = true }
+    },
+    opts_extend = { "sources.default" },
   },
 }
