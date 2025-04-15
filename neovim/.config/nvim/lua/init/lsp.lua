2e0fd8c23ed0fb1e64cb4b5f1cfee45b1f3cf037
Keith Thibodeaux <keith@railscoder.net>
2025-04-15 06:09:12 -0400

    vim: do not use virtual text for diagnostics

--- neovim/.config/nvim/lua/init/lsp.lua
+++ neovim/.config/nvim/lua/init/lsp.lua
@@ -5,13 +5,8 @@ vim.lsp.enable('rubocop')
 vim.lsp.enable('ruby_lsp')
 vim.lsp.enable('ts_ls')
 
-vim.diagnostic.config({
-  virtual_lines = {
-   current_line = true,
-  },
-})
-
 vim.api.nvim_set_keymap('n', '<leader>la', '<CMD> lua vim.lsp.buf.code_action()<CR>', { noremap = true, desc = 'code action' })
+vim.api.nvim_set_keymap('n', '<leader>ll', '<CMD> lua vim.diagnostic.open_float()<CR>', { noremap = true, desc = 'diagnostics' })
 vim.api.nvim_set_keymap('n', '<leader>lh', '<CMD> lua vim.lsp.buf.hover()<CR>', { noremap = true, desc = 'documentation' })
 vim.api.nvim_set_keymap('n', '<leader>ld', '<C-]>', { noremap = true, desc = 'go to definition' })
 vim.api.nvim_set_keymap('n', '<leader>lr', '<CMD> lua vim.lsp.buf.references()<CR>', { noremap = true, desc = 'references' })
