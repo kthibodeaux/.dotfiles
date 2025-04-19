7f74ea4075a2343fa1294e4d4b6663ffa731151c
Keith Thibodeaux <keith@railscoder.net>
2025-04-19 15:27:31 -0400

    vim: add blink-copilot

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -1,13 +1,24 @@
 return {
   {
     'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = {
+      'rafamadriz/friendly-snippets',
+      'fang2hou/blink-copilot'
+    },
     version = '1.*',
     opts = {
       keymap = { preset = 'enter' },
       completion = { documentation = { auto_show = true } },
       sources = {
-        default = { 'snippets', 'lsp', 'path', 'buffer' },
+        default = { 'snippets', 'lsp', 'path', 'buffer', 'copilot' },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
       },
       fuzzy = { implementation = "prefer_rust_with_warning" },
       signature = { enabled = true }
