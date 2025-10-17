9b6ea9557f054bd773ff9437ab8faaa7e8998dda
Keith Thibodeaux <keith@railscoder.net>
2025-10-17 08:56:43 -0400

    vim: add sidekick

--- neovim/.config/nvim/lua/plugins/ai.lua
+++ neovim/.config/nvim/lua/plugins/ai.lua
@@ -25,4 +25,52 @@ return {
       }
     },
   },
+  {
+    "folke/sidekick.nvim",
+    opts = {
+      cli = {
+        mux = {
+          backend = "tmux",
+          enabled = true,
+        },
+      },
+    },
+    keys = {
+      {
+        "<tab>",
+        function()
+          -- if there is a next edit, jump to it, otherwise apply it if any
+          if not require("sidekick").nes_jump_or_apply() then
+            return "<Tab>" -- fallback to normal tab
+          end
+        end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+      },
+      {
+        "<leader>it",
+        function()
+          require("sidekick.cli").toggle("claude")
+        end,
+        desc = "Sidekick Toggle CLI",
+      },
+      {
+        "<leader>ia",
+        function()
+          require("sidekick.cli").send({ msg = "{this}" })
+        end,
+        mode = { "x", "n" },
+        desc = "Send This",
+      },
+      {
+        "<leader>ip",
+        function()
+          require("sidekick.cli").prompt()
+        end,
+        mode = { "n", "x" },
+        desc = "Sidekick Select Prompt",
+      },
+    },
+  },
 }
+
