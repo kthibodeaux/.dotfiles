4b100e52e59a0e5edd0dec98feab8eab8f7b2c20
Keith Thibodeaux <keith@railscoder.net>
2025-10-21 12:57:26 -0400

    vim: do not allow modification of generated go files

--- neovim/.config/nvim/ftplugin/go.lua
+++ neovim/.config/nvim/ftplugin/go.lua
@@ -13,3 +13,12 @@ buffer_map(0, 'n', '<leader>ra', ':lua _G.tests.go.run_all()<CR>', { noremap = t
 buffer_map(0, 'n', '<leader>rl', ":lua _G.tests.run_last()<CR>", { noremap = true, desc = 'last test' })
 
 buffer_map(0, 'n', '<leader>.', ':lua require("open_alt").open()<CR>', { noremap = true, desc = 'open alt' })
+
+local first_lines = vim.api.nvim_buf_get_lines(0, 0, 10, false)
+for _, line in ipairs(first_lines) do
+	if line:lower():match("code generated") then
+		vim.opt_local.modifiable = false
+		vim.opt_local.readonly = true
+		break
+	end
+end
