4d6caba2cb53f4a97391a004e7cefec24b48ae71
Keith Thibodeaux <keith@railscoder.net>
2025-10-23 06:28:26 -0400

    vim: fix golangci-lint

--- neovim/.config/nvim/lsp/golangci_lint_ls.lua
+++ neovim/.config/nvim/lsp/golangci_lint_ls.lua
@@ -2,7 +2,7 @@ return {
   cmd = { 'golangci-lint-langserver' },
   filetypes = { 'go', 'gomod' },
   init_options = {
-    command = { "golangci-lint", "run", "--out-format", "json" }
+    command = { 'golangci-lint', 'run', '--output.json.path=stdout', '--show-stats=false' },
   },
   root_markers = { '.golangci.yml', '.golangci.yaml', '.golangci.toml', '.golangci.json', 'go.work', 'go.mod', '.git' },
 }
