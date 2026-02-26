0a5f73fe4f277ad3d151305e58a29bc7053bea26
Keith Thibodeaux <keith@railscoder.net>
2026-02-26 11:09:28 -0500

    vim: generated markdown is not editable

--- neovim/.config/nvim/ftplugin/markdown.lua
+++ neovim/.config/nvim/ftplugin/markdown.lua
@@ -1,2 +1,11 @@
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
+
+local first_lines = vim.api.nvim_buf_get_lines(0, 0, 10, false)
+for _, line in ipairs(first_lines) do
+	if line:lower():match("<!--") and line:lower():match("generated") then
+		vim.opt_local.modifiable = false
+		vim.opt_local.readonly = true
+		break
+	end
+end
