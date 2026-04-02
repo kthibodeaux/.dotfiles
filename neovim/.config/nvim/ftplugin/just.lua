d34c32c3a93c4d689c44955dbca27769d23083cb
Keith Thibodeaux <keith@railscoder.net>
2026-04-02 07:15:17 -0400

    vim: use tabs for justfile

---
+++ neovim/.config/nvim/ftplugin/just.lua
@@ -0,0 +1,6 @@
+vim.opt_local.expandtab = false
+vim.opt_local.softtabstop = 0
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.copyindent = true
+vim.opt_local.preserveindent = true
