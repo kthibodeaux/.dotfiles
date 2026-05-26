2f540541170bd291a8dace80641e3e379f016c02
Keith Thibodeaux <keith@railscoder.net>
2026-05-26 09:49:27 -0400

    vim: add lsp implementation bind

--- neovim/.config/nvim/lua/init/lsp.lua
+++ neovim/.config/nvim/lua/init/lsp.lua
@@ -17,4 +17,5 @@ vim.api.nvim_set_keymap('n', '<leader>ll', '<CMD> lua vim.diagnostic.open_float(
 vim.api.nvim_set_keymap('n', '<leader>lh', '<CMD> lua vim.lsp.buf.hover()<CR>', { noremap = true, desc = 'documentation' })
 vim.api.nvim_set_keymap('n', '<leader>ld', '<C-]>', { noremap = true, desc = 'go to definition' })
 vim.api.nvim_set_keymap('n', '<leader>lr', '<CMD> lua vim.lsp.buf.references()<CR>', { noremap = true, desc = 'references' })
+vim.api.nvim_set_keymap('n', '<leader>li', '<CMD> lua vim.lsp.buf.implementation()<CR>', { noremap = true, desc = 'implementations' })
 vim.api.nvim_set_keymap('n', '<leader>ln', '<CMD> lua vim.lsp.buf.rename()<CR>', { noremap = true, desc = 'rename' })
