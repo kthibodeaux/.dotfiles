cee029b9d2a53a77b2de09c68caab679b19dc1ea
Keith Thibodeaux <keith@railscoder.net>
2026-09-22 15:23:38 -0400

    host: add flatpak apps install script

---
+++ host/flatpak.sh
@@ -0,0 +1,9 @@
+# vi: set ft=sh :
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub org.freecad.FreeCAD
+flatpak install -y flathub org.kicad.KiCad
+flatpak install -y flathub com.discordapp.Discord
+flatpak install -y flathub com.slack.Slack
+flatpak install -y flathub org.signal.Signal
